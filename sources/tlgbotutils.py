8ab93327647b87e22f1a2922d3e00c881f019641
Chained Lupine <chainedlupine@gmail.com>
2021-11-21 21:25:22 -0600

    * Fixed TABs to SPACEs

--- sources/tlgbotutils.py
+++ sources/tlgbotutils.py
@@ -283,9 +283,9 @@ def tlg_restrict_user(bot, chat_id, user_id, until_date=None, timeout=None,
 
 
 def tlg_user_is_owner(bot, user):
-		'''Checks the User record to see if considered the bot owner.'''
+    '''Checks the User record to see if considered the bot owner.'''
     return (str(user.user_id) == CONST["BOT_OWNER"]) or \
-					 (user["username"] is not None and user.username == CONST["BOT_OWNER"])
+           (user["username"] is not None and user.username == CONST["BOT_OWNER"])
 
 def tlg_user_is_admin(bot, user_id, chat_id, timeout=None):
     '''Check if the specified user is an Administrator of a group given

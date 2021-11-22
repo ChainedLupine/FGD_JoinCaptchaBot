97b33252592ea1419c93f6380a928a7ddccf1531
Chained Lupine <chainedlupine@gmail.com>
2021-11-21 22:13:41 -0600

    This is getting stupid.

--- sources/tlgbotutils.py
+++ sources/tlgbotutils.py
@@ -284,7 +284,7 @@ def tlg_restrict_user(bot, chat_id, user_id, until_date=None, timeout=None,
 
 def tlg_user_is_owner(bot, user):
     '''Checks the User record to see if considered the bot owner.'''
-    return (str(user.user_id) == CONST["BOT_OWNER"]) or \
+    return (str(user.id) == CONST["BOT_OWNER"]) or \
            (user["username"] is not None and user.username == CONST["BOT_OWNER"])
 
 def tlg_user_is_admin(bot, user_id, chat_id, timeout=None):
